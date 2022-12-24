6563498cfef85183da2763768a3ffbaba790ea1a
MansouriNas <mansetudes@gmail.com>
2022-12-24 13:09:16 +0100

    Working 1st version of the game

--- src/main/java/com/example/demo/Bomb.java
+++ src/main/java/com/example/demo/Bomb.java
@@ -4,19 +4,19 @@ package com.example.demo;
 import javafx.scene.image.Image;
 
 import static com.example.demo.SpaceInvaders.HEIGHT;
+import static com.example.demo.SpaceInvaders.score;
 
 public class Bomb extends Rocket {
         int SPEED = (score/5)+2;
 
     public Bomb(int posX, int posY, int size, Image img) {
         super(posX, posY, size, img);
-
     }
 
 
     public void update(){
             super.update();
-            if(!exploding && !destoryed) posY += SPEED;
+            if(!exploding && !destoryed) posY += (score/5)+2;
             if(posY>HEIGHT) destoryed = true;
         }
     }
